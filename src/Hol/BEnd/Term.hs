db42a2babaff801328d3eafe5d10f2f9e1328718
KiJeong-Lim <gijungdduk@naver.com>
2023-07-04 22:26:43 +0900

    .

--- src/Hol/BEnd/Term.hs
+++ src/Hol/BEnd/Term.hs
@@ -9,10 +9,11 @@ type DbIdx = Int
 
 type Name = String
 
-data Term v c (a :: *)
+data Term v c a
     = LVar (v)
     | NIdx (DbIdx)
     | NCon (c)
     | NApp (Term v c a) (Term v c a)
     | NLam (Term v c a)
+    | NAnn a (Term v c a)
     deriving (Eq, Ord, Show, Functor)
--- src/Hol/BEnd/Unique.hs
+++ src/Hol/BEnd/Unique.hs
@@ -1,5 +1,5 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
-module Hol.BEnd.Base where
+module Hol.BEnd.Unique where
 
 import Control.Monad.IO.Class
 import Control.Monad.Trans.Class
