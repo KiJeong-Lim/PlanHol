feec78a6ad86a90779fa0ab83eff32027f39cb46
KiJeong-Lim <gijungdduk@naver.com>
2024-05-20 18:39:28 +0900

    .

--- src/TEST/Term2.hs
+++ src/TEST/Term2.hs
@@ -17,6 +17,8 @@ type IndividualVariableName = String
 
 type MetaVariableName = String
 
+type ReferenceName = String
+
 type SuspensionEnv = [SuspensionEnvItem]
 
 type Nat_ol = Nat
@@ -34,11 +36,11 @@ data Identifier name
     deriving (Eq, Ord, Show)
 
 data TermNode
-    = NVar (DeBruijnIndex)
+    = NIdx (DeBruijnIndex)
     | NCtr (Identifier DataConstructorName)
     | NApp (TermNode) (TermNode)
-    | NLam (Identifier IndividualVariableName)
-    | NFun (Identifier IndividualVariableName)
+    | NLam (TermNode)
+    | NFun (Identifier ReferenceName)
     | Meta (Identifier MetaVariableName) [TermNode]
     | Susp (TermNode) (Suspension)
     deriving (Eq, Ord, Show)
@@ -52,16 +54,113 @@ data Suspension
     = Suspension { _susp_ol :: Nat_ol, _susp_nl :: Nat_nl, _susp_env :: SuspensionEnv }
     deriving (Eq, Ord, Show)
 
-mkSusp :: TermNode -> Nat_ol -> Nat_nl -> SuspensionEnv -> TermNode
-mkSusp t ol nl env
+mkNIdx :: DeBruijnIndex -> TermNode
+mkNIdx i = if i >= 0 then NIdx i else error "***mkNIdx: A negative De-Bruijn index given..."
+
+mkNCtr :: Identifier DataConstructorName -> TermNode
+mkNCtr c = NCtr $! c
+
+mkNApp :: TermNode -> TermNode -> TermNode
+mkNApp t1 t2 = (NApp $! t1) $! t2
+
+mkNLam :: TermNode -> TermNode
+mkNLam t1 = NLam $! t1
+
+mkNFun :: Identifier ReferenceName -> TermNode
+mkNFun f = NFun $! f
+
+mkMeta :: Identifier MetaVariableName -> [TermNode] -> TermNode
+mkMeta x ts = (Meta $! x) $! ts
+
+mkSusp :: TermNode -> Suspension -> TermNode
+mkSusp t susp
     | NCtr {} <- t = t
-    | ol == 0 && nl == 0 = t
-    | otherwise = Susp t $! mkSuspension ol nl env
+    | Suspension ol nl env <- susp = if ol == 0 && nl == 0 then t else env `seq` Susp t susp
+
+buildSusp :: TermNode -> Nat_ol -> Nat_nl -> SuspensionEnv -> TermNode
+buildSusp t ol nl env = mkSusp t (mkSuspension ol nl env)
+
+addDummy :: Nat -> SuspensionEnv -> SuspensionEnv
+addDummy l env = l `seq` Dummy l : env
+
+addBinds :: TermNode -> Nat -> SuspensionEnv -> SuspensionEnv
+addBinds t l env = t `seq` l `seq` Binds t l : env
+
+emptySuspensionEnv :: SuspensionEnv
+emptySuspensionEnv = []
+
+nilSuspension :: Suspension
+nilSuspension = Suspension 0 0 []
 
 mkSuspension :: Nat_ol -> Nat_nl -> SuspensionEnv -> Suspension
 mkSuspension ol nl env
     | ol == length env && nl >= 0 = Suspension ol nl env 
     | otherwise = error "***mkSuspension: ol /= length env..."
+
+rewriteWithSusp :: TermNode -> Suspension -> ReductionOption -> TermNode
+rewriteWithSusp t susp option = dispatch t where
+    ol :: Nat_ol
+    ol = _susp_ol susp
+    nl :: Nat_nl
+    nl = _susp_nl susp
+    env :: SuspensionEnv
+    env = _susp_env susp
+    dispatch :: TermNode -> TermNode
+    dispatch (NIdx i)
+        | i >= ol = mkNIdx (i - ol + nl)
+        | i >= 0 = case env !! i of
+            Dummy l -> mkNIdx (nl - l)
+            Binds t l -> rewriteWithSusp t (mkSuspension 0 (nl - l) []) option
+        | otherwise = error "***rewriteWithSusp: A negative De-Bruijn index given..."
+    dispatch (NApp t1 t2)
+        = case t1' of
+            NLam t11 -> beta t11
+            _ -> reductionOption option
+        where
+            t1' :: TermNode
+            t1' = rewriteWithSusp t1 susp WHNF
+            beta :: TermNode -> TermNode
+            beta (Susp t' (Suspension ol' nl' (Dummy l' : env')))
+                | nl' == l' = rewriteWithSusp t' (mkSuspension ol' (pred nl') (addBinds (Susp t2 susp) (pred l') env')) option
+            beta t' = rewriteWithSusp t (mkSuspension 1 0 (addBinds (buildSusp t2 ol nl env) 0 emptySuspensionEnv)) option
+            reductionOption :: ReductionOption -> TermNode
+            reductionOption NF = mkNApp (rewriteWithSusp t1' nilSuspension option) (rewriteWithSusp t2 susp option)
+            reductionOption HNF = mkNApp (rewriteWithSusp t1' nilSuspension option) (Susp t2 susp)
+            reductionOption WHNF = mkNApp t1' (Susp t2 susp)
+    dispatch (NLam t1)
+        | option == WHNF = Susp t1 susp'
+        | otherwise = rewriteWithSusp t1 susp' option
+        where
+            susp' :: Suspension
+            susp' = mkSuspension (succ ol) (succ nl) (addDummy (succ nl) env)
+    dispatch (NFun f)
+        = mkNFun f
+    dispatch (Meta x ts)
+        = mkMeta x (map (rewrite NF) ts)
+    dispatch (Susp t' susp')
+        | ol' == 0 && nl' == 0 = rewriteWithSusp t' susp option
+        | ol == 0 = rewriteWithSusp t' (mkSuspension ol' (nl + nl') env') option
+        | otherwise = go (rewriteWithSusp t' susp' option)
+        where
+            ol' :: Nat_ol
+            ol' = _susp_ol susp'
+            nl' :: Nat_nl
+            nl' = _susp_nl susp'
+            env' :: SuspensionEnv
+            env' = _susp_env susp'
+            go :: TermNode -> TermNode
+            go (NLam t1)
+                | option == WHNF = mkNLam (Susp t1 susp1)
+                | otherwise = mkNLam (rewriteWithSusp t1 susp1 option)
+                where
+                    susp1 :: Suspension
+                    susp1 = mkSuspension (succ ol') (succ nl') (addDummy (succ nl') env')
+            go t = rewriteWithSusp t susp option
+    dispatch t
+        = mkSusp t susp
+
+rewrite :: ReductionOption -> TermNode -> TermNode
+rewrite option t = rewriteWithSusp t nilSuspension option
 
 unfoldNApp :: TermNode -> (TermNode, [TermNode])
 unfoldNApp = flip go [] where
