9ae5b0a7bad277a9860bd86af8a6d2879a5bdf9c
KiJeong-Lim <gijungdduk@naver.com>
2025-06-21 19:23:39 +0900

    .

--- src/Main.hs
+++ src/Main.hs
@@ -4,7 +4,12 @@ import qualified ALPHA1.Main as ALPHA1
 import qualified ALPHA2.Main as ALPHA2
 import qualified LGS.Main as LGS
 import qualified PGS.Main as PGS
+import System.Console.Pretty
 import Z.Utils
 
 main :: IO ()
-main = ALPHA2.main
+main = do
+    prettyable <- supportsPretty
+    putStr "colorable="
+    print prettyable
+    ALPHA2.main
